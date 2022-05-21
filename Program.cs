6e58e770e3ae1e94b2d32a911a962aa06410d69c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-05-22 08:25:10 +0900

    ブロードキャスト送信ひな形

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace UdpBroadcast
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpBroadcast
 {
     internal class Program
     {
+        static void SendBroadcastMessage(IPAddress targetAddress, string data)
+        {
+            // 送信元ポート
+            var src_port = 8000;
+
+            // 送信先ポート
+            var dst_port = 18000;
+
+            // 送信データ
+            var buffer = Encoding.UTF8.GetBytes(data);
+
+            // ブロードキャスト送信
+            var client = new UdpClient(src_port);
+
+            // ブロードキャスト有効化
+            client.EnableBroadcast = true;
+            client.Connect(new IPEndPoint(targetAddress, dst_port));
+            client.Send(buffer, buffer.Length);
+            client.Close();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var targetAddress = IPAddress.Parse("192.168.11.255");
+            SendBroadcastMessage(targetAddress, "Hello, World!");
         }
     }
 }
