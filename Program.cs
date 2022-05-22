72ecca368d4a1c18fd780d95f8988421942c6f0b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-05-22 13:07:30 +0900

    net5.0 に切り替える

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace UdpBroadcast
 {
