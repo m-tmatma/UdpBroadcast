efd8fb4340aba53decf5fe8fe4bcd2f4dc7bec83
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-05-22 20:16:11 +0900

    不要関数削除

--- Program.cs
+++ Program.cs
@@ -60,27 +60,6 @@ namespace UdpBroadcast
 
             yield break;
         }
-
-        static void SendBroadcastMessage(IPAddress targetAddress, string data)
-        {
-            // 送信元ポート
-            var src_port = 8000;
-
-            // 送信先ポート
-            var dst_port = 18000;
-
-            // 送信データ
-            var buffer = Encoding.UTF8.GetBytes(data);
-
-            // ブロードキャスト送信
-            var client = new UdpClient(src_port);
-
-            // ブロードキャスト有効化
-            client.EnableBroadcast = true;
-            client.Connect(new IPEndPoint(targetAddress, dst_port));
-            client.Send(buffer, buffer.Length);
-            client.Close();
-        }
         static Task<int> SendBroadcastMessage(UdpClient client, IPAddress targetAddress, string data)
         {
             // 送信先ポート
